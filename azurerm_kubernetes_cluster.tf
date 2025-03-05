f803a6f0d2b1a65cd9d84269c5d8a16b95b5f186
freddyyow <41685629+freddyyow@users.noreply.github.com>
2025-03-05 10:33:23 -0500

    Add files via upload

--- azurerm_kubernetes_cluster.tf
+++ azurerm_kubernetes_cluster.tf
@@ -1,12 +1,5 @@
-locals {
-  common_tags = {
-    DeployedWith = "Terraform",
-    Owner        = "PlatformEngineering"
-    Environment  = var.environment_map[var.target_environment]
-  }
-}
-
 resource "azurerm_kubernetes_cluster" "kubernetes_cluster" {
+  for_each                   = var.default_node_pool_settings
   name                       = "${var.project_name}-${var.environment_map[var.target_environment]}"
   location                   = var.location
   resource_group_name        = var.resource_group_name
@@ -14,30 +7,32 @@ resource "azurerm_kubernetes_cluster" "kubernetes_cluster" {
   private_cluster_enabled    = var.private_cluster_enabled
   automatic_upgrade_channel  = var.automatic_update_channel
   kubernetes_version         = var.kubernetes_version
-  sku_tier                   = var.target_environment == "PROD" ? "Standard" : "Free"
-  #automatic_upgrade_channel  = var.env == "prod" ? "stable" : "rapid"
-  # sku_tier                   = var.env == "prod" ? "Standard" : "Free"
-  #   auto_scaler_profile {
+  sku_tier                   = var.environment_map[var.target_environment] == "prod" ? "Standard" : "Free"
+
+  default_node_pool {
+    name                 = "default-node-pool"
+    auto_scaling_enabled = each.value.auto_scaling_enabled
+    node_count           = each.value
+    min_count            = each.value.min_count
+    max_count            = each.value.max_count
+    vm_size              = each.value.vm_size
+    node_labels          = each.value.node_labels
+    os_sku               = each.value.os_sku
+  }
+  # dynamic "default_node_pool" {
+  #   for_each = var.default_node_pool_settings
+  #   #iterator = "test"
+
+  #   content {
+  #     name                 = default_node_pool.key
+  #     auto_scaling_enabled = var.default_node_pool_settings.auto_scaling_enabled
+  #     node_count = default_node_pool.value.default_node_pool_settings.node_count
+  #     min_count  = default_node_pool.value.min_count
+  #     max_count  = default_node_pool.value.max_count
+  #     vm_size    = default_node_pool.value.vm_size
 
   #   }
-  dynamic "default_node_pool" {
-    for_each = var.default_node_pool_settings
-    #iterator = "test"
-
-    content {
-      name                 = default_node_pool.key
-      auto_scaling_enabled = var.default_node_pool_settings.auto_scaling_enabled
-      # node_count           = var.node_count
-      # min_count            = var.min_count
-      # max_count            = var.max_count
-      # vm_size              = var.vm_size
-      node_count = default_node_pool.value.default_node_pool_settings.node_count
-      min_count  = default_node_pool.value.min_count
-      max_count  = default_node_pool.value.max_count
-      vm_size    = default_node_pool.value.vm_size
-      #vm_size              = var.env == "prod" ? var.default_pool_vm_size : "Standard_DS2_v2"
-    }
-  }
+  # }
 
   identity {
     type = "SystemAssigned"
@@ -49,3 +44,40 @@ resource "azurerm_kubernetes_cluster" "kubernetes_cluster" {
     ignore_changes = [default_node_pool[0].node_count, kubernetes_version, tags]
   }
 }
+
+resource "azurerm_kubernetes_cluster_node_pool" "pool" {
+  for_each = var.node_pool_settings
+
+  name                  = each.key
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.kubernetes_cluster[*].id
+  vm_size               = each.value.vm_size
+  auto_scaling_enabled  = each.value.auto_scaling_enabled
+  node_count            = each.value.node_count
+  min_count             = each.value.min_count
+  max_count             = each.value.max_count
+  priority              = each.value.priority
+  vnet_subnet_id        = var.vnet_subnet_id
+  os_type               = each.value.os_type
+  os_sku                = each.value.os_sku
+  node_taints           = each.value.node_taints
+
+  dynamic "linux_os_config" {
+    for_each = var.linux_os_config
+
+    content {
+      swap_file_size_mb = linux_os_config.value.swap_file_size_mb
+
+
+      dynamic "sysctl_config" {
+        for_each = var.sysctl_config
+
+        content {
+          vm_max_map_count = sysctl_config.value.vm_max_map_count
+          vm_swappiness    = sysctl_config.value.vm_swappiness
+        }
+      }
+    }
+  }
+  tags = merge(var.tags, local.common_tags)
+}
+
